1d0b9e39089519faf0fe25ae094bdd6bb175f554
PVM-75 <vperevalov@gmail.com>
2024-09-03 12:31:33 +0800

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Владимир"
+print("Имя: " + name)
+age = 48
+print("Возраст:", age, "лет")
+age = age + 1
+print("Новый возраст:", age, "лет")
+is_student = True
+print("Студент?: ", is_student)
